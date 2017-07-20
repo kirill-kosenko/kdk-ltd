3c7390f63faacce7c522eae9e2b8872380e4df84
Kosenko Kirill <nikeya444@gmail.com>
2017-07-20 19:17:49 +0300

    tests added

---
+++ src/test/java/kdk/ltd/site/root/services/impl/DealServiceImplTest.java
@@ -0,0 +1,142 @@
+package kdk.ltd.site.root.services.impl;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import kdk.ltd.config.RootContextConfiguration;
+import kdk.ltd.site.root.dto.DealDTO;
+import kdk.ltd.site.root.entities.Deal;
+import kdk.ltd.site.root.entities.DealDetail;
+import kdk.ltd.site.root.entities.GenericDeal;
+import kdk.ltd.site.root.entities.ProductInStock;
+import kdk.ltd.site.root.repositories.*;
+import kdk.ltd.site.root.services.DealService;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration( classes = RootContextConfiguration.class )
+@Sql( executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = "classpath:insert_deals.sql" )
+@Transactional
+public class DealServiceImplTest {
+
+    @Inject
+    DealService<Deal, DealDTO> service;
+
+    @Inject
+    ProductRepository productRepository;
+    @Inject
+    PartnerRepository partnerRepository;
+    @Inject
+    UserRepository userRepository;
+    @Inject
+    StorageRepository storageRepository;
+    @Inject
+    ProductInStockRepository productInStockRepository;
+
+    @Inject
+    ObjectMapper objectMapper;
+
+    Pageable pageable = new PageRequest(1, 3);
+
+    Deal deal;
+
+    @Before
+    public void setUp() {
+        DealDetail detail1 =
+                new DealDetail(productRepository.getOne(3L), 50, new BigDecimal(12000));
+        DealDetail detail2 =
+                new DealDetail(productRepository.getOne(4L), 10, new BigDecimal(1200));
+        deal =
+                new Deal(
+                        GenericDeal.Type.PURCHASE,
+                        partnerRepository.getOne(1L),
+                        true,
+                        LocalDate.now(),
+                        Arrays.asList(detail1, detail2)
+                );
+    }
+
+    @Test
+    public void findTest() {
+        Deal deal = service.find(1L);
+        Assert.assertNotNull(deal);
+    }
+
+    @Test
+    public void findAllTest() {
+        List<Deal> deals = service.findAll();
+        Assert.assertEquals(5, deals.size());
+    }
+
+    @Test
+    public void findDtoTest() {
+        DealDTO dto = service.findDto(5L);
+        Assert.assertNotNull(dto);
+    }
+
+    @Test
+    public void pageFindAllTest() {
+        Page<DealDTO> page = service.findAll(pageable);
+        Assert.assertEquals(3, page.getSize());
+        Assert.assertEquals(5, page.getTotalElements());
+        Assert.assertEquals(2, page.getTotalPages());
+    }
+
+    @Test
+    public void saveTest() {
+        service.save(deal);
+        Deal actual = service.find(6L);
+
+        Assert.assertEquals(deal, actual);
+    }
+
+    @Test
+    public void saveAllTest() {
+
+    }
+
+    @Test
+    public void searchTest() {
+
+    }
+
+    @Test
+    public void whenSavesDeal_updatesProductInStock_thenCorrect() {
+        DealDetail detail1 =
+                new DealDetail(productRepository.getOne(3L), 50, new BigDecimal(12000), storageRepository.getOne(1L));
+
+        Deal deal =
+                new Deal(
+                        GenericDeal.Type.PURCHASE,
+                        partnerRepository.getOne(1L),
+                        true,
+                        LocalDate.now(),
+                        Collections.singletonList(detail1)
+                );
+
+        service.save(deal);
+
+        ProductInStock inStock = productInStockRepository.findOne(1L);
+
+        Assert.assertEquals(detail1.getStorage().getId(), inStock.getStorage().getId());
+        Assert.assertEquals(detail1.getProduct().getId(), inStock.getProduct().getId());
+        Assert.assertEquals(new Integer(50), inStock.getQuantity());
+        Assert.assertEquals(new BigDecimal(12000).negate(), inStock.getSum());
+    }
+}
---
+++ src/test/java/kdk/ltd/site/root/services/impl/ProductInStockServiceImplTest.java
@@ -0,0 +1,119 @@
+package kdk.ltd.site.root.services.impl;
+
+
+import kdk.ltd.config.RootContextConfiguration;
+import kdk.ltd.site.root.dto.DealDTO;
+import kdk.ltd.site.root.entities.Deal;
+import kdk.ltd.site.root.entities.DealDetail;
+import kdk.ltd.site.root.entities.ProductInStock;
+import kdk.ltd.site.root.repositories.PartnerRepository;
+import kdk.ltd.site.root.repositories.ProductRepository;
+import kdk.ltd.site.root.repositories.StorageRepository;
+import kdk.ltd.site.root.repositories.UserRepository;
+import kdk.ltd.site.root.services.DealService;
+import kdk.ltd.site.root.services.ProductInStockService;
+import kdk.ltd.site.root.services.exceptions.NegativeBalanceException;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+@RunWith( SpringJUnit4ClassRunner.class )
+@ContextConfiguration( classes = RootContextConfiguration.class)
+@Sql( executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = "classpath:stock.sql" )
+@Transactional
+public class ProductInStockServiceImplTest {
+
+    @Inject
+    ProductInStockService productInStockService;
+
+    @PersistenceContext
+    EntityManager em;
+
+    @Inject
+    DealService<Deal, DealDTO> service;
+
+    @Inject
+    ProductRepository productRepository;
+    @Inject
+    PartnerRepository partnerRepository;
+    @Inject
+    UserRepository userRepository;
+    @Inject
+    StorageRepository storageRepository;
+
+    @Test
+    public void findOneTest() {
+        ProductInStock inStock = productInStockService.findOne(1L);
+        Assert.assertNotNull(inStock);
+    }
+
+    @Test
+    public void findByTest() {
+        ProductInStock inStock = productInStockService.findBy(3L, 4L);
+        Assert.assertEquals(new Long(1), inStock.getId());
+    }
+
+    @Test
+    public void findByStorageTest() {
+        List<ProductInStock> list = productInStockService.findByStorage(4L);
+        Assert.assertEquals(2, list.size());
+    }
+
+    @Test
+    public void findByProductTest() {
+        List<ProductInStock> list = productInStockService.findByProduct(3L);
+        Assert.assertEquals(1L, list.size());
+    }
+
+    @Test
+    public void updateProductsInStockTest() {
+        DealDetail detail1 =
+                new DealDetail(productRepository.getOne(3L), 50, new BigDecimal(-12000), storageRepository.getOne(4L));
+
+        DealDetail detail2 =
+                new DealDetail(productRepository.getOne(3L), -40, new BigDecimal(12000), storageRepository.getOne(4L));
+        productInStockService.updateProductsInStock(Arrays.asList(detail1, detail2));
+        em.flush();
+        ProductInStock inStock = productInStockService.findOne(1L);
+
+        Assert.assertEquals(new Integer(13), inStock.getQuantity());
+        Assert.assertEquals(new BigDecimal(19200), inStock.getSum());
+    }
+
+    @Test(expected = NegativeBalanceException.class)
+    public void updateProductsInStockExceptionTest() {
+        DealDetail detail =
+                new DealDetail(productRepository.getOne(3L), -10, new BigDecimal(3200), storageRepository.getOne(4L));
+
+        productInStockService.updateProductsInStock(Collections.singletonList(detail));
+    }
+
+    @Test
+    public void testPurchaseUpdate() {
+
+    }
+
+    @Test
+    public void testCreatePeriod() {
+
+    }
+
+    @Test
+    public void testDeleteAll() {
+
+    }
+
+
+}
