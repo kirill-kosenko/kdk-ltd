6a9a213650aa6d39f7cba10624ced7546f71b0e8
Kosenko Kirill <nikeya444@gmail.com>
2017-07-20 19:30:48 +0300

    redundant methods deleted

--- src/main/java/kdk/ltd/site/root/repositories/ProductInStockRepository.java
+++ src/main/java/kdk/ltd/site/root/repositories/ProductInStockRepository.java
@@ -15,23 +15,15 @@ import java.util.Optional;
 
 public interface ProductInStockRepository extends JpaRepository<ProductInStock, Long> {
 
-    @Query("select r.id from ProductInStock r where r.product = ?1 and r.storage = ?2 and r.restDate = ?3")
-    Long findId(Product product, Storage storage, LocalDate date);
-    @Modifying
-    @Query("update ProductInStock r set r.quantity = r.quantity + ?1, r.sum = r.sum + ?2 where r.id = ?3")
-    void addQuantityAndSum(Integer qnt, BigDecimal summ, Long id);
-
     Optional<ProductInStock> findByProductIdAndStorageId(Long productId, Long storageId);
     @Query("select new ProductInStock(r.id, r.restDate, r.storage, r.product, r.quantity, r.sum) from ProductInStock r where r.product = ?1 and r.storage = ?2 and r.restDate = ?3")
     Optional<ProductInStock> findBy(Product product, Storage storage, LocalDate date);
-    Optional<ProductInStock> findByProductAndStorageAndRestDate(Product product, Storage storage, LocalDate date);
-    Optional<ProductInStock> findByProductIdAndStorageIdAndRestDate(Long productId, Long storageId, LocalDate date);
+    ProductInStock findByProductAndStorageAndRestDate(Product product, Storage storage, LocalDate date);
+
     List<ProductInStock> findByStorageId(Long storageId);
     List<ProductInStock> findByProductId(Long productId);
     @Query("select sum(r.quantity) from ProductInStock r where r.product.id = ?1")
     Integer findReamainingQuantityForProduct(Long productId);
-    @Query("select sum(r.quantity) from ProductInStock r where r.storage.id = ?1")
-    Integer findRemainingQuantityForStorage(Long storageId);
 
     @Query("select new ProductInStock(dd.storage, dd.product, sum(dd.quantity), sum(dd.sum) ) " +
             "from DealDetail dd where dd.document.id in (?1)" +
